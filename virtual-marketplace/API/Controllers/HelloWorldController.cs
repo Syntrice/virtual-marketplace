d7cc4654df363f39d5887e3a92564ddb1b15a162
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-01-07 22:19:39 +0000

    feat: Add example placeholder controller and endpoint
    Add an example / placeholder controller HelloWorldController, and endpoint
    returning OkObjectResult. This is to test that everything is working correctly.

---
+++ virtual-marketplace/API/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VirtualMarketplace.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult HelloWorld()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
