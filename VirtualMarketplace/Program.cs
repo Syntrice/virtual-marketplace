b95127555b64e467641fc805efd8099b83732496
Samuel M <76839403+Syntrice@users.noreply.github.com>
2025-01-08 14:56:18 +0000

    feat: Add ApplicationDbContext IdentityDbContext
    Implement the IdentityDbContext with IdentityUser, in the ApplicationDbContext
    class. Load the connections string and add the database context to services in
    Program.

--- VirtualMarketplace/Program.cs
+++ VirtualMarketplace/Program.cs
@@ -1,8 +1,20 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using VirtualMarketplace.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var applicationDbConnectionString = builder.Configuration.GetConnectionString("ApplicationDb");
+
+if (applicationDbConnectionString == null)
+{
+    throw new InvalidOperationException("ApplicationDb connection string not found.");
+}
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(applicationDbConnectionString));
 
 var app = builder.Build();
 
